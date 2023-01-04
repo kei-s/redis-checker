d1651ca0426e19fd1a2c2518f0f4fac2798917ef
kei-s <kei.shiratsuchi@gmail.com>
2023-01-04 21:28:24 +0900

    Use connection pool, split check and log

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use bb8_redis::{bb8::Pool, RedisConnectionManager};
 use futures::{stream::TryStreamExt, StreamExt};
 use redis::{AsyncCommands, RedisResult};
 use std::hash::Hasher;
 use tokio::{fs::OpenOptions, io::AsyncWriteExt};
 
-const DEFAULT_CONCURRENCY: usize = 50;
+const DEFAULT_REDIS_CONNECTION: u32 = 30;
+const DEFAULT_CONCURRENCY: usize = 200;
 
 #[tokio::main]
 async fn main() {
@@ -12,9 +14,21 @@ async fn main() {
     opts.reqopt("h", "host", "Redis host: redis://127.0.0.1/", "HOST");
     opts.reqopt("o", "output", "Output log file", "OUTPUT");
     opts.optopt(
-        "c",
-        "concurrency",
-        &format!("concurrent number: default {DEFAULT_CONCURRENCY}"),
+        "",
+        "redis_connection",
+        &format!("redis connection number {DEFAULT_REDIS_CONNECTION}"),
+        "NUM",
+    );
+    opts.optopt(
+        "",
+        "c_redis",
+        &format!("concurrent number for redis: default {DEFAULT_CONCURRENCY}"),
+        "NUM",
+    );
+    opts.optopt(
+        "",
+        "c_file",
+        &format!("concurrent number for file: default {DEFAULT_CONCURRENCY}"),
         "NUM",
     );
     let matches = opts
@@ -22,8 +36,14 @@ async fn main() {
         .expect(&opts.usage(&format!("Usage: {}", &args[0])));
     let redis_url = matches.opt_str("h").unwrap();
     let log_path = matches.opt_str("o").unwrap();
-    let concurency = matches
-        .opt_get_default::<usize>("c", DEFAULT_CONCURRENCY)
+    let redis_connection = matches
+        .opt_get_default::<u32>("redis_connection", DEFAULT_REDIS_CONNECTION)
+        .unwrap();
+    let concurency_redis = matches
+        .opt_get_default::<usize>("c_redis", DEFAULT_CONCURRENCY)
+        .unwrap();
+    let concurency_file = matches
+        .opt_get_default::<usize>("c_file", DEFAULT_CONCURRENCY)
         .unwrap();
 
     // reset log file
@@ -37,39 +57,66 @@ async fn main() {
 
     println!("Start... version: {:}", env!("CARGO_PKG_VERSION"));
 
-    let keys = get_keys(&redis_url).await.unwrap();
-    check_values(&redis_url, &log_path, keys, concurency)
+    let manager = RedisConnectionManager::new(redis_url).unwrap();
+    let pool = Pool::builder()
+        .max_size(redis_connection)
+        .build(manager)
         .await
         .unwrap();
+
+    check_all(pool.clone(), &log_path, concurency_redis, concurency_file).await;
 }
 
-async fn get_keys(redis_url: &str) -> RedisResult<Vec<String>> {
+async fn check_all(
+    pool: Pool<RedisConnectionManager>,
+    log_path: &str,
+    concurrency_redis: usize,
+    concurrency_file: usize,
+) {
+    let keys = get_keys(pool.clone()).await.unwrap();
+    check_values(
+        pool.clone(),
+        &log_path,
+        concurrency_redis,
+        concurrency_file,
+        keys,
+    )
+    .await
+    .unwrap();
+}
+
+async fn get_keys(pool: Pool<RedisConnectionManager>) -> RedisResult<Vec<String>> {
     println!("Start getting all keys");
-    let client = redis::Client::open(redis_url)?;
-    let mut con = client.get_async_connection().await?;
+    let mut con = pool.get().await.unwrap();
     let keys = con.keys("*").await?;
     Ok(keys)
 }
 
 async fn check_values(
-    redis_url: &str,
+    pool: Pool<RedisConnectionManager>,
     log_path: &str,
+    concurrency_redis: usize,
+    concurrency_file: usize,
     keys: Vec<String>,
-    concurrency: usize,
 ) -> RedisResult<()> {
     println!("Start checking all values");
     futures::stream::iter(keys)
-        .map(|key| check_and_log(redis_url, log_path, key))
-        .buffered(concurrency)
+        .map(|key| check(pool.clone(), key))
+        .buffered(concurrency_redis)
+        .map(|x| x.unwrap())
+        .map(|(key, hash_name, hash)| log(log_path, key, hash_name, hash))
+        .buffered(concurrency_file)
         .try_collect::<()>()
         .await?;
     Ok(())
 }
 
-async fn check_and_log(redis_url: &str, log_path: &str, key: String) -> RedisResult<()> {
-    let client = redis::Client::open(redis_url)?;
-    let mut con = client.get_async_connection().await?;
-    let type_name: String = redis::cmd("TYPE").arg(&key).query_async(&mut con).await?;
+async fn check(
+    pool: Pool<RedisConnectionManager>,
+    key: String,
+) -> RedisResult<(String, String, u64)> {
+    let mut con = pool.get().await.unwrap();
+    let type_name: String = redis::cmd("TYPE").arg(&key).query_async(&mut *con).await?;
     let result: Vec<String> = match &*type_name {
         "string" => vec![con.get(&key).await?],
         "set" => {
@@ -94,6 +141,10 @@ async fn check_and_log(redis_url: &str, log_path: &str, key: String) -> RedisRes
     };
     let hash = calculate_hash(&result);
 
+    Ok((key, type_name, hash))
+}
+
+async fn log(log_path: &str, key: String, type_name: String, hash: u64) -> RedisResult<()> {
     let mut file = OpenOptions::new()
         .create(true)
         .append(true)
